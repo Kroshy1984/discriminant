e29ea9994da1eabc19e3fbd2ed8f78ffcfa13ea9
Ваше Имя <raznomazov_vm@mail.ru>
2020-01-06 20:55:04 +0200

    the new version of class was predicted2

--- Discriminant.py
+++ Discriminant.py
@@ -1,8 +1,8 @@
 class Discriminant ():
     def __init__(self,a,b,c):
-        self.a=a
-        self.b=b
-        self.c=c
+        self.a=int(a)
+        self.b=int(b)
+        self.c=int(c)
         self.equal=str(a)+'x'+chr(178)+'+'+str(b)+'x'+'+'+str(c)+'=0'
         self.Discriminant=self.b**2-4*self.a*self.c
 		
