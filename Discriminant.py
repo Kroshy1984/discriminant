b304fbceabfbde58ea4ff99b07da0fef122bb4bf
Ваше Имя <raznomazov_vm@mail.ru>
2020-01-06 21:06:32 +0200

    the new version of class was predicted3

--- Discriminant.py
+++ Discriminant.py
@@ -4,5 +4,12 @@ class Discriminant ():
         self.b=int(b)
         self.c=int(c)
         self.equal=str(a)+'x'+chr(178)+'+'+str(b)+'x'+'+'+str(c)+'=0'
+        self.equal=self.equal.replace('-1x','-')
+        self.equal=self.equal.replace('+1x','+')
+        self.equal=self.equal.replace('1'+'x'+chr(178),'x'+chr(178))
+        self.equal=self.equal.replace('0'+'x'+chr(178)+'+','')
+        self.equal=self.equal.replace('x+0','x')
+        self.equal=self.equal.replace('+0x','+')
+        self.equal=self.equal.replace('++','+')
+        self.equal=self.equal.replace('+-','-')
         self.Discriminant=self.b**2-4*self.a*self.c
-		
